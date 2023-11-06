999ed0342420dd1c6c539dcb88fe078e0b170518
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2023-11-05 21:29:32 -0400

    add pre-prod stage

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { EBApplnStack } from '../lib/hawkstatus-stack';
+import { CdkPipelineStack } from '../lib/pipeline-stack';
 
 const app = new cdk.App();
-new EBApplnStack(app, 'HawkstatusStack', {
+new CdkPipelineStack(app, 'CdkPipelineStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
   },
 });
+
+app.synth();
