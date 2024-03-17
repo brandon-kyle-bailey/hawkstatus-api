b41044fec9815a185d26327c321e1cf16eed83a6
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2024-03-17 03:28:34 -0300

    add client url to redirect on verify

--- src/interface/controllers/user/verify-user.http.controller.ts
+++ src/interface/controllers/user/verify-user.http.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Logger, Query, Req } from '@nestjs/common';
+import { Controller, Get, Logger, Query, Redirect } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { VeriyUserRequestDto } from '../../dtos/user/verfiy-user.request.dto';
 import { UpdateUserCommand } from '../../commands/user/update-user.command';
 import { UpdateUserRequestDto } from '../../dtos/user/update-user.request.dto';
 import { UserMapper } from 'src/infrastructure/mappers/user.mapper';
+import configuration from '@app/common/config/configuration';
 
 @Controller('v1')
 export class VeriyUserController {
@@ -14,16 +15,16 @@ export class VeriyUserController {
   ) {}
 
   @Get('user/verify')
-  async verify(
-    @Query() query: VeriyUserRequestDto,
-    @Req() request: any,
-  ): Promise<void> {
+  @Redirect('https://docs.nestjs.com', 302)
+  async verify(@Query() query: VeriyUserRequestDto): Promise<{ url: string }> {
     try {
       const command = UpdateUserCommand.create({
         userId: query.id,
         verified: true,
       } as UpdateUserRequestDto);
-      const result = await this.commandBus.execute(command);
+      await this.commandBus.execute(command);
+      const { client_url } = configuration().web;
+      return { url: `${client_url}/signin` };
     } catch (error) {
       this.logger.error(
         'VeriyUserController.verify encountered an error',
